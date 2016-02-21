9cfd4de6813857f4cf1943ee6ff2a727b5a6ea7e
mv740 <michalwozniak@live.ca>
2016-02-21 09:34:52 -0500

    first api get request

--- server.js
+++ server.js
@@ -4,8 +4,24 @@
 
 var http = require('http');
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var express = require('express');
+var app = express();
+var path = require("path");
 
+var rootDirectoryPath = {root: __dirname + '/../../'};
+
+
+/* GET home page. */
+app.get('/', function (req, res) {
+    res.sendFile('index.html', { root: path.join(__dirname, '/public') });
+
+});
+
+
+app.get('/hello', function (req, res) {
+    res.send({"hello":"world"})
+});
+
+http.createServer(app).listen(port, function () {
+    console.log('HTTPS Server Started!');
+});
